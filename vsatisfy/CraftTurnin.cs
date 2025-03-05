b30681c61fcc77ea11449b912f39d3f91a647995
Hagazzi Viliana <hagazzi.viliana@gmail.com>
2025-03-05 23:29:20 +0100

    try again

--- vsatisfy/CraftTurnin.cs
+++ vsatisfy/CraftTurnin.cs
@@ -18,9 +18,6 @@ public sealed class CraftTurnin
     public uint VendorShopId;
     public ulong TurnInInstanceId;
     public Vector3 TurnInLocation;
-    //private static int jobid;
-    //private static readonly IClientState _clientState;
-    public static readonly IClientState myjobid;
 
     public CraftTurnin(uint supplyId, uint turnInENPCId, uint territoryId)
     {
@@ -28,7 +25,6 @@ public sealed class CraftTurnin
         // note: we assume that all ingredients are sold by the same vendor of the same shop in the same territory as turn-in npc
         var craftedItemId = Service.LuminaRow<SatisfactionSupply>(supplyId, 0)!.Value.Item.RowId;
         var ingredientId = GetCraftIngredient(craftedItemId).id;
-        //jobid = (int)_clientState.LocalPlayer!.ClassJob.RowId;
         string scene = Service.LuminaRow<TerritoryType>(territoryId)!.Value.Bg.ToString();
         var filenameStart = scene.LastIndexOf('/') + 1;
         var planeventLayerGroup = "bg/" + scene[0..filenameStart] + "planevent.lgb";
@@ -67,94 +63,64 @@ public sealed class CraftTurnin
     // TODO: job selection
     public static uint GetRecipeId(uint craftedItemId)
     {
-        //Carpenter = 8,
-        //Blacksmith = 9,
-        //Armorer = 10,
-        //Goldsmith = 11,
-        //Leatherworker = 12,
-        //Weaver = 13,
-        //Alchemist = 14,
-        //Culinarian = 15,
-        //Dalamud.Plugin.Services.IClientState myjobid;
-        int number = (int)myjobid.LocalPlayer!.ClassJob.RowId;
-        switch (number)
+        var currentjob = Plugin.ClientState.LocalPlayer!.ClassJob.RowId;
+        switch (currentjob)
         {
             case 8:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.CRP.RowId ?? 0;
             case 9:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.BSM.RowId ?? 0;
             case 10:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.ARM.RowId ?? 0;
             case 11:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.GSM.RowId ?? 0;
             case 12:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.LTW.RowId ?? 0;
             case 13:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.WVR.RowId ?? 0;
             case 14:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.ALC.RowId ?? 0;
             case 15:
-                // code block
                 return Service.LuminaRow<RecipeLookup>(craftedItemId)?.CUL.RowId ?? 0;
             default:
-                // code block
                 return 0;
         }
-
-        
     }
 
     public static (uint id, int count) GetCraftIngredient(uint craftedItemId)
     {
-        int number = (int)myjobid.LocalPlayer!.ClassJob.RowId;
+        var currentjob = Plugin.ClientState.LocalPlayer!.ClassJob.RowId;
         var recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.CUL.Value;
-        switch (number)
+        switch (currentjob)
         {
             case 8:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.CRP.Value;
                 break;
             case 9:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.BSM.Value;
                 break;
             case 10:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.ARM.Value;
                 break;
             case 11:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.GSM.Value;
                 break;
             case 12:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.LTW.Value;
                 break;
             case 13:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.WVR.Value;
                 break;
             case 14:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.ALC.Value;
                 break;
             case 15:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.CUL.Value;
                 break;
             default:
-                // code block
                 recipe = Service.LuminaRow<RecipeLookup>(craftedItemId)?.CUL.Value;
                 break;
         }
-        
         return recipe != null ? (recipe.Value.Ingredient[0].RowId, recipe.Value.AmountIngredient[0]) : default;
     }
 
